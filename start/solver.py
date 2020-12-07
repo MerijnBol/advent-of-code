66d47ddb002d02ddd8313b5cebb50695d8eea27c
merijn <merijn@merijnbol.nl>
2020-12-07 20:00:00 +0100

    SOlution day 7 and the default folder.

---
+++ start/solver.py
@@ -0,0 +1,29 @@
+
+def build_():
+    f = open("input.txt", "r")
+    groups = []
+    for i in f.read().split("\n\n"):
+        entry = []
+        for j in i.split("\n"):
+            entry.append(j)        
+        groups.append(entry)
+    return groups
+
+
+
+def puzzle_1():
+    
+    print(f"""
+    Puzzle number 1:
+
+    """)
+
+def puzzle_2():
+
+    print(f"""
+    Puzzle number 2:
+
+    """)
+
+puzzle_1()
+puzzle_2()
