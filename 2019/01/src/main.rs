52929d7d84598e26602e36a661d8925a46b11c96
Merijn Bol <merijn@merijnbol.nl>
2023-10-13 19:20:56 +0200

    Finish puzzle 1

--- 2019/01/src/main.rs
+++ 2019/01/src/main.rs
@@ -2,12 +2,9 @@ use std::env;
 use std::fs;
 
 fn main() {
-    // let masses = [12, 14, 1969, 100756];
-    // println!("Total fuel load: {} kg", calculate_fuel(masses))
-
-    let path: String = create_path_to_file("test_input.txt");
-
-    read_input(&path);
+    let path: String = create_path_to_file("input.txt");
+    let masses = read_input(&path);
+    println!("{}", calculate_fuel(masses));
 }
 
 fn counter_upper(mass: u32) -> u32 {
@@ -17,7 +14,7 @@ fn counter_upper(mass: u32) -> u32 {
     return float as u32;
 }
 
-fn calculate_fuel(masses: [u32; 4]) -> u32 {
+fn calculate_fuel(masses: Vec<u32>) -> u32 {
     let mut total = 0;
     for mass in masses.iter() {
         total += counter_upper(*mass)
@@ -28,14 +25,17 @@ fn calculate_fuel(masses: [u32; 4]) -> u32 {
 fn create_path_to_file(filename: &str) -> String {
     let mut working_dir = env::current_dir().expect("Get current dir");
     working_dir.push(filename);
-    println!(
-        "testing: {}",
-        working_dir.to_str().expect("Must have a path!")
-    );
     return working_dir.to_str().expect("Must have a path!").into();
 }
 
-fn read_input(path: &str) {
+fn read_input(path: &str) -> Vec<u32> {
     let content = fs::read_to_string(path).expect("Read file");
-    println!("Content is: \n\n{}", content);
+    let mut mass_array = Vec::new();
+    for raw in content.split("\n") {
+        let mass = raw.parse::<u32>();
+        if mass.is_ok() {
+            mass_array.push(raw.parse::<u32>().unwrap())
+        }
+    }
+    return mass_array;
 }
