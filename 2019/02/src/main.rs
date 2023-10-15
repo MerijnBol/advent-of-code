0f921f09fa81cf02a45b3372252dfc84a2aaf07b
Merijn Bol <merijn@merijnbol.nl>
2023-10-15 15:43:38 +0200

    Work on puzzle 2

--- 2019/02/src/main.rs
+++ 2019/02/src/main.rs
@@ -2,10 +2,44 @@ use std::env;
 use std::fs;
 
 fn main() {
-    let path: String = create_path_to_file("input.txt");
-    let ints = read_input(&path);
+    // let path: String = create_path_to_file("input.txt");
+    // let ints = read_input(&path);
 
-    dbg!(ints);
+    let intcode = vec![1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50];
+    puzzle_one(intcode);
+}
+
+fn puzzle_one(intcode: Vec<u32>) {
+    let mut intcode = intcode;
+    intcode = handle_opcode(1, intcode);
+    dbg!(intcode);
+}
+
+fn handle_opcode(index: usize, intcode: Vec<u32>) -> Vec<u32> {
+    let mut intcode = intcode;
+
+    let target_index_1 = intcode[index + 1] as usize;
+    let target_index_2 = intcode[index + 2] as usize;
+    let target_index_3 = intcode[index + 3] as usize;
+    if intcode[index] == 1 {
+        intcode[target_index_3] = intcode[target_index_1] + intcode[target_index_2];
+    } else if intcode[index] == 2 {
+        intcode[target_index_3] = intcode[target_index_1] * intcode[target_index_2];
+    }
+    intcode
+}
+
+#[test]
+fn it_handles_addition() {
+    let intcode = vec![1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50];
+    let expected = vec![1, 9, 10, 70, 2, 3, 11, 0, 99, 30, 40, 50];
+    assert_eq!(handle_opcode(0, intcode), expected);
+}
+#[test]
+fn it_handles_multiplication() {
+    let intcode = vec![1, 9, 10, 70, 2, 3, 11, 0, 99, 30, 40, 50];
+    let expected = vec![3500, 9, 10, 70, 2, 3, 11, 0, 99, 30, 40, 50];
+    assert_eq!(handle_opcode(4, intcode), expected);
 }
 
 fn create_path_to_file(filename: &str) -> String {
@@ -23,5 +57,6 @@ fn read_input(path: &str) -> Vec<u32> {
             intcode.push(integer.unwrap());
         }
     }
+    let intcode = intcode;
     return intcode;
 }
