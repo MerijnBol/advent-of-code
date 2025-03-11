3fa4961a130b7f0b042eea10efc996ec86afde18
Merijn Bol <merijn@merijnbol.nl>
2025-03-11 20:46:43 +0100

    improve code style

--- 2024/Day02/Main.java
+++ 2024/Day02/Main.java
@@ -11,9 +11,9 @@ public class Main {
             List<String> input = Files.readAllLines(Paths.get("Day02/input.txt"));
 
             List<List<Integer>> reports = parseInput(input);
-            Integer safeCount = 0;
+            int safeCount = 0;
             for (int i = 0; i < reports.size(); i++) {
-                reportResult result = reportSafe(reports.get(i));
+                ReportResult result = reportSafe(reports.get(i));
                 if (result.isSafe) {
                     safeCount++;
                 }
@@ -25,27 +25,27 @@ public class Main {
             // Any failing report gets stored as a second chance. We need 2
             // lists since removing either of the 2 failing positions could make
             // the report valid.
-            List<List<Integer>> second_chance_a = new ArrayList<>();
-            List<List<Integer>> second_chance_b = new ArrayList<>();
+            List<List<Integer>> secondChanceA = new ArrayList<>();
+            List<List<Integer>> secondChanceB = new ArrayList<>();
             for (int i = 0; i < reports.size(); i++) {
-                reportResult result = reportSafe(reports.get(i));
+                ReportResult result = reportSafe(reports.get(i));
                 if (result.isSafe) {
                     safeCount++;
                 } else {
                     // Add the 2 second chances
-                    List<Integer> report_a = new ArrayList<>(reports.get(i));
-                    List<Integer> report_b = new ArrayList<>(reports.get(i));
-                    report_a.remove(result.position);
-                    report_b.remove(result.position + 1);
-                    second_chance_a.add(report_a);
-                    second_chance_b.add(report_b);
+                    List<Integer> reportA = new ArrayList<>(reports.get(i));
+                    List<Integer> reportB = new ArrayList<>(reports.get(i));
+                    reportA.remove(result.position);
+                    reportB.remove(result.position + 1);
+                    secondChanceA.add(reportA);
+                    secondChanceB.add(reportB);
                 }
             }
             // Try the second chances one more time. If one of them is safe, we count it.
-            for (int i = 0; i < second_chance_a.size(); i++) {
-                reportResult result_a = reportSafe(second_chance_a.get(i));
-                reportResult result_b = reportSafe(second_chance_b.get(i));
-                if (result_a.isSafe || result_b.isSafe) {
+            for (int i = 0; i < secondChanceA.size(); i++) {
+                ReportResult resultA = reportSafe(secondChanceA.get(i));
+                ReportResult resultB = reportSafe(secondChanceB.get(i));
+                if (resultA.isSafe || resultB.isSafe) {
                     safeCount++;
                 }
             }
@@ -56,17 +56,17 @@ public class Main {
         }
     }
 
-    public static class reportResult {
+    public static class ReportResult {
         public final boolean isSafe;
         public final int position;
 
-        public reportResult(boolean isSafe, int position) {
+        public ReportResult(boolean isSafe, int position) {
             this.isSafe = isSafe;
             this.position = position;
         }
     }
 
-    public static reportResult reportSafe(List<Integer> report) {
+    public static ReportResult reportSafe(List<Integer> report) {
         int midpoint = report.size() / 2;
         int skippoint = report.size() - report.size() / 2;
         Integer firstHalve = report.stream().limit(midpoint).mapToInt(Integer::intValue).sum();
@@ -81,14 +81,15 @@ public class Main {
         for (int i = 0; i < report.size() - 1; i++) {
             Integer gap = report.get(i + 1) - report.get(i);
             if (!validGap(gap, direction)) {
-                return new reportResult(false, i);
+                return new ReportResult(false, i);
             }
         }
-        return new reportResult(true, -1);
+        return new ReportResult(true, -1);
     }
 
-    public static Boolean validGap(Integer gap, Integer direction) {
-        if (Math.abs(gap) == 0 || Math.abs(gap) > 3 || direction * gap < 0) {
+    public static boolean validGap(int gap, int direction) {
+        final int MAX_ALLOWED_GAP = 3;
+        if (Math.abs(gap) == 0 || Math.abs(gap) > MAX_ALLOWED_GAP || direction * gap < 0) {
             return false;
         }
         return true;
